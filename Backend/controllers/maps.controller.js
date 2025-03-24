c1055ee32d891610c37a743ab5dcaba137abe059
Shashwat <shashwatgoyal2004@gmail.com>
2025-03-24 12:35:24 +0530

    add maps functionality with routes for coordinates and distance/time; integrate axios for API calls

---
+++ Backend/controllers/maps.controller.js
@@ -0,0 +1,42 @@
+const mapsService = require('../services/maps.service.js');
+const { validationResult } = require('express-validator');
+
+
+module.exports.getCordinates = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { address } = req.query;
+
+    try {
+        const coordinates = await mapsService.getAddressCoordinates(address);
+
+        return res.status(200).json({ coordinates });
+    } catch (error) {
+        res.status(404).json({ message: 'Coordinates not found' });
+    }
+}
+
+
+module.exports.getDistanceTime = async (req, res, next) => {
+
+    try {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { origin, destination } = req.query;
+
+        const distanceTime = await mapsService.getDistanceTime(origin, destination);
+
+        return res.status(200).json(distanceTime);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
